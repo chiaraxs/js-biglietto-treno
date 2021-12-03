68c839ed26b82be2df4a41dbad4b0f0a40ed20b9
chiaraxs <chiaracardinale92@gmail.com>
2021-12-03 15:37:53 +0100

    aggiunte prime variabili

--- js/script.js
+++ js/script.js
@@ -4,10 +4,21 @@
 // Il programma dovrà chiedere all’utente il numero di chilometri che vuole percorrere e l’età del passeggero.
 // 1. CREARE DUE PROMPT CONST ALL'AVVIO CON RICHIESTA ETA' E KM DA VOLER PERCORRERE.
 
+const YourAge = prompt('Quanti anni hai?');
+const YourKm = prompt('Quanti km vuoi percorrere?');
+
+
 // --------STEP 2--------- //
 // Sulla base di queste informazioni dovrà calcolare il prezzo totale del viaggio, secondo queste regole:
 // il prezzo del biglietto è definito in base ai km (0.21 € al km).
 // 2. CREARE VARIABILE CHE DEFINISCE IL PREZZO DEL BIGLIETTO IN BASE AL PREZZO IN KM NEL RANGE INDICATO.
+
+let kmPrice = 0.21;
+
+// moltiplico kmPrice x YourKm per definire costo passeggero//
+
+let personalTotalPrice = kmPrice * YourKm;
+
 
 // --------STEP 3--------- //
 // va applicato uno sconto del 20% per i minorenni
@@ -23,3 +34,4 @@
 
 // --------STEP 6--------- //
 // 6. STAMPARE I DATI RELATIVI AL BIGLIETTO (CON SCONTO ETA') NEL DOM.
+const outputHtml = document.getElementById('price');
