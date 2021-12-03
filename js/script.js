60d1e7210e2ce8511892339c08e00b32f118f55e
chiaraxs <chiaracardinale92@gmail.com>
2021-12-03 18:05:27 +0100

    versione finale

--- js/script.js
+++ js/script.js
@@ -19,9 +19,6 @@ const kmPrice = 0.21;
 
 let personalTotalPrice = kmPrice * yourKm;
 
-// L’output del prezzo finale va messo fuori in forma umana (con massimo due decimali, per indicare centesimi sul prezzo.
-// USO TO.FIXED CON VALORE 2 //
-personalTotalPrice = personalTotalPrice.toFixed(2);
 
 // --------STEP 3--------- //
 // va applicato uno sconto del 20% per i minorenni e uno sconto del 40% per gli over 65.
@@ -33,14 +30,17 @@ let saleForty = personalTotalPrice * 0.4;
 // --------STEP 4-5--------- //
 // 4. STAMPARE I DATI RELATIVI AL BIGLIETTO (CON SCONTO ETA') NEL DOM.
 // 5. CREO ISTRUZ. CONDIZIONALI E AGGIUNGO VALORI CHE DEFINISCONO GLI SCONTI, DA AGGIUNGERE AL PREZZO TOT FINALE IN BASE ALL'ETA'.
+// L’output del prezzo finale va messo fuori in forma umana (con massimo due decimali, per indicare centesimi sul prezzo.
+// 6. USO TO.FIXED CON VALORE 2 //
+
 
 const outputHtml = document.getElementById('personalTotalPrice');
 
 if (yourAge <= 18)  {
-    outputHtml.innerHTML = "Il prezzo del tuo biglietto è: " + (personalTotalPrice - saleTwenty) + "€";
+    outputHtml.innerHTML = "Il prezzo del tuo biglietto è: " + (personalTotalPrice.toFixed(2) - saleTwenty) + "€";
 } else if (yourAge > 65) {
-    outputHtml.innerHTML = "Il prezzo del tuo biglietto è: " + (personalTotalPrice - saleForty) + "€";
+    outputHtml.innerHTML = "Il prezzo del tuo biglietto è: " + (personalTotalPrice.toFixed(2) - saleForty) + "€";
 } else {
-    outputHtml.innerHTML = "Il prezzo del tuo biglietto è: " + personalTotalPrice + " €" ;
+    outputHtml.innerHTML = "Il prezzo del tuo biglietto è: " + personalTotalPrice.toFixed(2) + " €" ;
 }
     
